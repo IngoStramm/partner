4d15493d94b5a6e7fe4f7d9a7ea8e138e874fbe9
Ingo Stramm <ingostramm@gmail.com>
2022-11-22 08:08:09 -0300

    fix popup style

--- partner.php
+++ partner.php
@@ -4,7 +4,7 @@
  * Plugin Name: Partner
  * Plugin URI: https://agencialaf.com
  * Description: Descrição do Partner.
- * Version: 0.0.17
+ * Version: 0.0.18
  * Author: Ingo Stramm
  * Text Domain: partner
  * License: GPLv2
