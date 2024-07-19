d2d0c646b1547bda383e9128a5393caf5f406fd2
Ingo Stramm <ingostramm@gmail.com>
2024-07-19 10:51:01 -0300

    fix minor css changes

--- partner.php
+++ partner.php
@@ -4,7 +4,7 @@
  * Plugin Name: Partner
  * Plugin URI: https://agencialaf.com
  * Description: Descrição do Partner.
- * Version: 1.3.2
+ * Version: 1.3.3
  * Author: Ingo Stramm
  * Text Domain: partner
  * License: GPLv2
